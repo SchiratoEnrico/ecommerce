ac1bae33263c482e7d2eeaff6b81ec8dc0b0e485
SchiratoEnrico <97945290+SchiratoEnrico@users.noreply.github.com>
2026-03-30 19:59:20 +0200

    Revert "Feature/ordini"

--- src/app/models/manga.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Manga } from './mangamodel';
-
-describe('Manga', () => {
-  it('should create an instance', () => {
-    expect(new Manga()).toBeTruthy();
-  });
-});
--- src/app/models/pagamenti.model.ts
+++
@@ -1,5 +0,0 @@
-export class Pagamenti {
-    id: number;
-    tipoPagamento: string;
-    
-}
